9df27a54a0ab727831b224edb6c789e67d351f0f
CoolDuke <me@coolduke.com>
2016-04-01 09:34:13 +0200

    now copies apache certs even if not existing

--- update_apache_certs.sh
+++ update_apache_certs.sh
@@ -19,38 +19,42 @@ for sslcert in $sslcerts; do
   #ignore cert if not controlled by letsencrypt
   [ -e $LETSENCRYPT_CERTDIR/$domain.crt ] || continue
 
-  #ignore cert if md5 sums match or are empty
-  oldcert_md5sum=$(md5sum $sslcert | cut -d" " -f1)
-  newcert_md5sum=$(md5sum $LETSENCRYPT_CERTDIR/$domain.crt | cut -d" " -f1)
-  if [ -z $oldcert_md5sum ] || [ -z $newcert_md5sum ] || [ "$oldcert_md5sum" == "$newcert_md5sum" ]; then
-    continue
+  #if cert already exists do some checks
+  if [ -e $sslcert ]; then
+    #ignore cert if md5 sums match or are empty
+    oldcert_md5sum=$(md5sum $sslcert | cut -d" " -f1)
+    newcert_md5sum=$(md5sum $LETSENCRYPT_CERTDIR/$domain.crt | cut -d" " -f1)
+    if [ -z $oldcert_md5sum ] || [ -z $newcert_md5sum ] || [ "$oldcert_md5sum" == "$newcert_md5sum" ]; then
+      continue
+    fi
+  
+    #get common names of old and new certificates
+    oldcert_cn=$(openssl x509 -noout -subject -in $sslcert | cut -d= -f3)
+    [ $? -eq 0 ] || { >&2 echo "Error while getting common name of $sslcert"; continue; }
+    newcert_cn=$(openssl x509 -noout -subject -in $LETSENCRYPT_CERTDIR/$domain.crt | cut -d= -f3)
+    [ $? -eq 0 ] || { >&2 echo "Error while getting common name of $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
+  
+    #check if common names match domain name
+    [ "$oldcert_cn" == "$domain" ] || { >&2 echo "CN of old certificate $oldcert_cn does not match domain name $domain"; continue; }
+    [ "$newcert_cn" == "$domain" ] || { >&2 echo "CN of new certificate $newcert_cn does not match domain name $domain"; continue; }
+  
+    #get expiration dates of old and new certificates
+    oldcert_enddate_string=$(openssl x509 -noout -enddate -in $sslcert | cut -d= -f2; exit ${PIPESTATUS[0]})
+    [ $? -eq 0 ] || { >&2 echo "Error while getting enddate of $sslcert"; continue; }
+    newcert_enddate_string=$(openssl x509 -noout -enddate -in $LETSENCRYPT_CERTDIR/$domain.crt | cut -d= -f2; exit ${PIPESTATUS[0]})
+    [ $? -eq 0 ] || { >&2 echo "Error while getting enddate of $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
+  
+    #convert expiration dates to timestamp
+    oldcert_enddate=$(date --date="$oldcert_enddate_string" "+%s")
+    [ $? -eq 0 ] || { >&2 echo "Error getting timestamp of $oldcert_enddate_string"; continue; }
+    newcert_enddate=$(date --date="$newcert_enddate_string" "+%s")
+    [ $? -eq 0 ] || { >&2 echo "Error getting timestamp of $newcert_enddate_string"; continue; }
+  
+    #compare expiration dates
+    [ $oldcert_enddate -gt $newcert_enddate ] && { >&2 echo "Old certificate $sslcert expires after new certificate $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
+
   fi
-
-  #get common names of old and new certificates
-  oldcert_cn=$(openssl x509 -noout -subject -in $sslcert | cut -d= -f3)
-  [ $? -eq 0 ] || { >&2 echo "Error while getting common name of $sslcert"; continue; }
-  newcert_cn=$(openssl x509 -noout -subject -in $LETSENCRYPT_CERTDIR/$domain.crt | cut -d= -f3)
-  [ $? -eq 0 ] || { >&2 echo "Error while getting common name of $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
-
-  #check if common names match domain name
-  [ "$oldcert_cn" == "$domain" ] || { >&2 echo "CN of old certificate $oldcert_cn does not match domain name $domain"; continue; }
-  [ "$newcert_cn" == "$domain" ] || { >&2 echo "CN of new certificate $newcert_cn does not match domain name $domain"; continue; }
-
-  #get expiration dates of old and new certificates
-  oldcert_enddate_string=$(openssl x509 -noout -enddate -in $sslcert | cut -d= -f2; exit ${PIPESTATUS[0]})
-  [ $? -eq 0 ] || { >&2 echo "Error while getting enddate of $sslcert"; continue; }
-  newcert_enddate_string=$(openssl x509 -noout -enddate -in $LETSENCRYPT_CERTDIR/$domain.crt | cut -d= -f2; exit ${PIPESTATUS[0]})
-  [ $? -eq 0 ] || { >&2 echo "Error while getting enddate of $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
-
-  #convert expiration dates to timestamp
-  oldcert_enddate=$(date --date="$oldcert_enddate_string" "+%s")
-  [ $? -eq 0 ] || { >&2 echo "Error getting timestamp of $oldcert_enddate_string"; continue; }
-  newcert_enddate=$(date --date="$newcert_enddate_string" "+%s")
-  [ $? -eq 0 ] || { >&2 echo "Error getting timestamp of $newcert_enddate_string"; continue; }
-
-  #compare expiration dates
-  [ $oldcert_enddate -gt $newcert_enddate ] && { >&2 echo "Old certificate $sslcert expires after new certificate $LETSENCRYPT_CERTDIR/$domain.crt"; continue; }
-
+  
   #check if new key belongs to new certificate by comparing their md5 hashes
   newcert_md5hash=$(openssl x509 -in $LETSENCRYPT_CERTDIR/$domain.crt -noout -modulus | openssl md5)
   newkey_md5hash=$(openssl rsa -in $LETSENCRYPT_CERTDIR/$domain.key -noout -modulus 2>/dev/null | openssl md5)
